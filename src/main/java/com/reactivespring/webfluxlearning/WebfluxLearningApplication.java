30030b5dd8cf4fb35a36f7eda84d4bd411fd8b19
Sai Abhilash Abhay Wadegaonkar <saiabhilash.wadegaonkar@publicissapient.com>
2021-04-11 20:12:48 +0530

    basic-setup

---
+++ src/main/java/com/reactivespring/webfluxlearning/WebfluxLearningApplication.java
@@ -0,0 +1,13 @@
+package com.reactivespring.webfluxlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reactivespring/webfluxlearning/WebfluxLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactivespring.webfluxlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
